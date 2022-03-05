4299d3d50cc2e4e6ef597742fe4163d0d82f7658
sanjra <sanjrabezzi@gmail.com>
2022-03-05 17:16:18 +1100

    initial react setup
    removal of unnecessary items
    personal customisation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        Sanjras Weather App
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://app.netlify.com/teams/sanjra/overview"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          netlify.teams.sanjra
         </a>
       </header>
     </div>
