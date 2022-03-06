77a606807164e84eb11497eb929008e2b802a0e4
sanjra <sanjrabezzi@gmail.com>
2022-03-06 16:19:34 +1100

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Sanjras Weather App</header>
-      <footer>
-        <p>This project was coded by sanjra while learning React.</p>
-        <a
-          className="App-link"
-          href="https://github.com/sanjra/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          github.sanjra.weather
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">Sanjras Weather App</header>
+        <button className="btn btn-primary"></button>
+        <footer>
+          <p>This project was coded by sanjra while learning React.</p>
+          <a
+            className="App-link"
+            href="https://github.com/sanjra/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.sanjra.weather
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
