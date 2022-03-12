b69400234757918be20917b6843c38ac0094615d
sanjra <sanjrabezzi@gmail.com>
2022-03-12 14:37:27 +1100

    REWORK

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 <<<<<<< HEAD
 import React from "react";
+<<<<<<< HEAD
 import Weather from "./Weather.js";
 =======
 >>>>>>> parent of 77a6068 (bootstrap added)
+=======
+>>>>>>> parent of 1471f5f (Simponised)
 import "./App.css";
 
 export default function App() {
@@ -10,19 +13,22 @@ export default function App() {
     <div className="App">
 <<<<<<< HEAD
       <div className="container">
+<<<<<<< HEAD
         <Weather defaultCity="Melbourne" />
+=======
+        <header className="App-header">Sanjras Weather App</header>
+        <button className="btn btn-primary"></button>
+>>>>>>> parent of 1471f5f (Simponised)
         <footer>
-          <p>
-            This project was coded by sanjra while learning React.
-            <a
-              className="App-link"
-              href="https://github.com/sanjra/react-weather-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              github.sanjra.weather
-            </a>
-          </p>
+          <p>This project was coded by sanjra while learning React.</p>
+          <a
+            className="App-link"
+            href="https://github.com/sanjra/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.sanjra.weather
+          </a>
         </footer>
       </div>
 =======
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import "./Weather.js";
 import "bootstrap/dist/css/bootstrap.css";
 =======
 >>>>>>> parent of 77a6068 (bootstrap added)
+=======
+>>>>>>> parent of 1471f5f (Simponised)
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
