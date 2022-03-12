68882274a290d5e6c189b02ed78fd6cc1e126615
sanjra <sanjrabezzi@gmail.com>
2022-03-12 14:15:11 +1100

    setting up axios and api calls

--- src/Weather.js
+++ src/Weather.js
@@ -1,48 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      city: response.data.name,
+      country: response.data.city.country,
+      date: "Wednesday 11 11:11",
+      description: response.data.weather[0].description,
+      temperature: response.data.main.temp,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      weather_icon: response.data.weather.weather_icon,
+      feels_like: response.data.main.feels_like,
+      temp_min: response.data.main.temp_min,
+      temp_max: response.data.main.temp_max,
+      wind: response.data.main.wind.speed,
+      humidity: response.data.main.humidty,
+    });
+
+    setTemperature();
+
+    setReady(true);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-dark" />
+            </div>
+          </div>
+        </form>
+        <h1>
+          {weatherData.city}
+          {weatherData.date}
+        </h1>
+        <h2>{weatherData.country}</h2>
+        <ul>
+          <li>{weatherData}</li>
+          <li class="text-capitalize">{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="VENT RADIOACTIVE GAS?"
-              className="form-control"
+          <div className="col-6">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              className="float-left"
             />
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">℃</span>
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Y / N" className="btn btn-dark" />
+          <div className="col-6">
+            <ul>
+              <li>Feels Like: {weatherData.feels_like}</li>
+              <li>Temp Min: {weatherData.temp_min}</li>
+              <li>Temp Max: {weatherData.temp_max}</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind}km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>Sydney</h1>
-      <h2>NSW, AUS 11:11</h2>
-      <ul>
-        <li>Sunday 6th Smarch 2022</li>
-        <li>Partly Cloudy with a chance of meatballs</li>
-        <li>Lousy Smarch Weather</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Partly Cloudy"
-          />
-          6°C
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Preciptiation: 15%</li>
-            <li>Humidity: 100%</li>
-            <li>
-              Wind: <em>comin' in from the south</em>
-            </li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "244beb54e63857bfaf6d34f213aa412a";
+    let city = "Melbourne";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading....";
+  }
 }
