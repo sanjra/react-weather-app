12d2144bf6fad0c600f865c8cc5425f8f918f7f6
sanjra <sanjrabezzi@gmail.com>
2022-03-12 15:37:22 +1100

    Updates afterCrash

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import "./App.css";
 
 export default function App() {
-      <div className="container">
-        <header className="App-header">Sanjras Weather App</header>
-        <button className="btn btn-primary"></button>
-        <footer>
-          <p>This project was coded by sanjra while learning React.</p>
-          <a
-            className="App-link"
-            href="https://github.com/sanjra/react-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            github.sanjra.weather
-          </a>
-        </footer>
-      </div>
-    </div>
-  );
+  <div className="container">
+    <header className="App-header">Sanjras Weather App</header>
+    <button className="btn btn-primary"></button>
+    <footer>
+      <p>This project was coded by sanjra while learning React.</p>
+      <a
+        className="App-link"
+        href="https://github.com/sanjra/react-weather-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        github.sanjra.weather
+      </a>
+    </footer>
+  </div>;
 }
-
